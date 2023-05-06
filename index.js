3fd4052260aa4eee2e605038651bc67cdaab292e
Gomathy <gomathy2511@gmail.com>
2023-05-06 18:21:26 +0530

    modif

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ db();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-   origin: 'http://localhost:3000',
-   credentials:true
+   origin: '*',
+   
+  
 }
 ));
 app.use(express.urlencoded({ extended: true }));
